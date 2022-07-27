74b79f830efe9d568582f5f7aa04662f6cdf5436
Ulan <ulankdt@gmail.com>
2022-07-27 19:49:36 +0600

    Creatable Entity

--- devlet-catalog/src/main/java/org/startup/devletcatalog/DevletCatalogApplication.java
+++ devlet-catalog/src/main/java/org/startup/devletcatalog/DevletCatalogApplication.java
@@ -2,8 +2,11 @@ package org.startup.devletcatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DevletCatalogApplication {
 
     public static void main(String[] args) {
